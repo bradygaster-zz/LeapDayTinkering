1dd8b6bec0e8db681da498c00961a81ecdfa2d10
Brady Gaster <bradyg@microsoft.com>
2016-02-11 14:30:39 -0800

    regenerated code following controller change

---
+++ LeapDayTinkering.AppService/Controllers/DeviceController.cs
@@ -0,0 +1,35 @@
+﻿using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace LeapDayTinkering.AppService.Controllers
+{
+    public class DeviceController : ApiController
+    {
+        [ResponseType(typeof(bool))]
+        [SwaggerResponse(HttpStatusCode.Conflict, "A device with this ID has already been recorded.", typeof(bool))]
+        [SwaggerResponse(HttpStatusCode.Created, "The device ID was recorded successfully.", typeof(bool))]
+        public HttpResponseMessage Post(string deviceId)
+        {
+            if(DoesDeviceAlreadyExist(deviceId))
+            {
+                return Request.CreateResponse<bool>(HttpStatusCode.Conflict, false);
+            }
+            else
+            {
+                return Request.CreateResponse<bool>(HttpStatusCode.Created, true);
+            }
+        }
+
+        private bool DoesDeviceAlreadyExist(string deviceId)
+        {
+            // todo: implement
+            return false;
+        }
+    }
+}
