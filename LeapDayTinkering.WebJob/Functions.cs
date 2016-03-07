65871233ec7cdbfb796fdae33067bfa4a3d93ae5
Brady Gaster <bradyg@microsoft.com>
2016-03-07 00:32:18 -0800

    added table storage to webjob

--- LeapDayTinkering.WebJob/Functions.cs
+++ LeapDayTinkering.WebJob/Functions.cs
@@ -6,12 +6,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Newtonsoft.Json.Linq;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace LeapDayTinkering.WebJob
 {
     public class Functions
     {
-        public static void ProcessQueueMessage([QueueTrigger("incoming")] string message, TextWriter log)
+        public static void ProcessQueueMessage([QueueTrigger("incoming")] string message, 
+            [Table("sensorreadings")] ICollector<SensorReading> tableBinding,
+            TextWriter log)
         {
             JObject sensorReading = JObject.Parse(message);
 
@@ -19,7 +22,26 @@ namespace LeapDayTinkering.WebJob
             string sensorName = (string)sensorReading["SensorName"];
             double value = (double)sensorReading["Value"];
 
+            var tableEntity = new SensorReading
+            {
+                DeviceId = deviceId,
+                PartitionKey = "Test",
+                SensorName = sensorName,
+                Timestamp = DateTime.Now,
+                Value = value,
+                RowKey = Guid.NewGuid().ToString()
+            };
+
+            tableBinding.Add(tableEntity);
+
             log.WriteLine($"Received {value} from Sensor {sensorName} on device {deviceId}");
         }
+    }
+
+    public class SensorReading : TableEntity
+    {
+        public string DeviceId { get; set; }
+        public string SensorName { get; set; }
+        public double Value { get; set; }
     }
 }
